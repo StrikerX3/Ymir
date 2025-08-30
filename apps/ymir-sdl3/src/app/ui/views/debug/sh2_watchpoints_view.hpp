c426f96850578f82e2c222dad3bb2ce5e7b5c3c7
StrikerX3 <StrikerX3@users.noreply.github.com>
2025-08-30 10:46:42 -0300

    chore(debug): Leave all watchpoint checkboxes enabled by default

--- apps/ymir-sdl3/src/app/ui/views/debug/sh2_watchpoints_view.hpp
+++ apps/ymir-sdl3/src/app/ui/views/debug/sh2_watchpoints_view.hpp
@@ -15,12 +15,12 @@ private:
     ymir::sh2::SH2 &m_sh2;
 
     uint32 m_address = 0x00000000;
-    bool m_read8 = false;
-    bool m_read16 = false;
-    bool m_read32 = false;
-    bool m_write8 = false;
-    bool m_write16 = false;
-    bool m_write32 = false;
+    bool m_read8 = true;
+    bool m_read16 = true;
+    bool m_read32 = true;
+    bool m_write8 = true;
+    bool m_write16 = true;
+    bool m_write32 = true;
 };
 
 } // namespace app::ui
