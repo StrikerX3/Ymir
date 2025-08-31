4bb79884fc5559cfc397a173392e19108f612cb2
StrikerX3 <StrikerX3@users.noreply.github.com>
2025-08-31 12:11:47 -0300

    chore: Describe what the `ConfigureWindowDecorations` does

--- apps/ymir-sdl3/src/util/os_features.hpp
+++ apps/ymir-sdl3/src/util/os_features.hpp
@@ -4,6 +4,8 @@
 
 namespace util::os {
 
+// Changes window decorations depending on the operating system:
+// - Windows 11: disables rounded corners
 void ConfigureWindowDecorations(SDL_Window *window);
 
 } // namespace util::os
