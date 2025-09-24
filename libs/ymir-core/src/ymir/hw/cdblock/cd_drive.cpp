4fe56d056f1122d2e1c1891f25a58639906d0c14
StrikerX3 <StrikerX3@users.noreply.github.com>
2025-09-24 12:28:13 -0300

    chore(CDBlock): Disable/silence spammy CD drive logs

--- libs/ymir-core/src/ymir/hw/cdblock/cd_drive.cpp
+++ libs/ymir-core/src/ymir/hw/cdblock/cd_drive.cpp
@@ -331,7 +331,7 @@ FORCE_INLINE uint64 CDDrive::OpReadSector() {
         return kDriveCyclesPlaying1x / m_readSpeed;
     }
 
-    devlog::debug<grp::lle_cd>("Read sector {:06X}", m_currFAD);
+    devlog::trace<grp::lle_cd>("Read sector {:06X}", m_currFAD);
 
     const auto &session = m_disc.sessions.back();
     const auto *track = session.FindTrack(m_currFAD);
