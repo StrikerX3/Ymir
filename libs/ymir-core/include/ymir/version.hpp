5f1c69f97adc98ff63d4d0048f7be8f2cdde3492
StrikerX3 <StrikerX3@users.noreply.github.com>
2025-10-11 21:51:52 -0300

    feat(build): Define build type flags (stable, nightly, dev, local)

--- libs/ymir-core/include/ymir/version.hpp
+++ libs/ymir-core/include/ymir/version.hpp
@@ -7,9 +7,20 @@
 
 #if Ymir_DEV_BUILD
     #define Ymir_FULL_VERSION Ymir_VERSION "-dev"
+    #define Ymir_DEV_BUILD 1
+    #ifdef Ymir_BUILD_TIMESTAMP
+        #define Ymir_NIGHTLY_BUILD 1
+    #else
+        #define Ymir_NIGHTLY_BUILD 0
+    #endif
 #else
     #define Ymir_FULL_VERSION Ymir_VERSION
+    #define Ymir_DEV_BUILD 0
+    #define Ymir_NIGHTLY_BUILD 0
 #endif
+
+#define Ymir_STABLE_BUILD !Ymir_DEV_BUILD
+#define Ymir_LOCAL_BUILD (Ymir_DEV_BUILD && !Ymir_NIGHTLY_BUILD)
 
 namespace ymir::version {
 
@@ -30,5 +41,19 @@ inline constexpr auto minor = static_cast<unsigned>(Ymir_VERSION_MINOR); ///< Th
 inline constexpr auto patch = static_cast<unsigned>(Ymir_VERSION_PATCH); ///< The library's patch version
 inline constexpr auto prerelease = Ymir_VERSION_PRERELEASE;              ///< The library's prerelease version
 inline constexpr auto build = Ymir_VERSION_BUILD;                        ///< The library's build version
+
+/// @brief Whether this is a development build.
+/// This is only ever `false` for stable releases.
+inline constexpr bool is_dev_build = Ymir_DEV_BUILD;
+
+/// @brief Whether this is a nightly build.
+/// `false` means it's either a stable build or a local build.
+inline constexpr bool is_nightly_build = Ymir_NIGHTLY_BUILD;
+
+/// @brief Whether this is a stable build.
+inline constexpr bool is_stable_build = Ymir_STABLE_BUILD;
+
+/// @brief Whether this is a local build (neither stable nor nightly).
+inline constexpr bool is_local_build = Ymir_LOCAL_BUILD;
 
 } // namespace ymir::version
