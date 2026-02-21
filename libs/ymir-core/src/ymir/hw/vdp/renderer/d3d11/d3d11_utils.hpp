f536b1ffc3dff44c52b48b6d96f67a8b0565eef2
StrikerX3 <StrikerX3@users.noreply.github.com>
2026-02-21 19:04:11 -0300

    chore(d3d11): Refactor resource creation for easier management

--- libs/ymir-core/src/ymir/hw/vdp/renderer/d3d11/d3d11_utils.hpp
+++ libs/ymir-core/src/ymir/hw/vdp/renderer/d3d11/d3d11_utils.hpp
@@ -1,12 +1,31 @@
 #pragma once
 
+#include <ymir/util/dev_assert.hpp>
 #include <ymir/util/inline.hpp>
 
 #include <d3d11.h>
 
+#include <array>
+#include <bit>
 #include <concepts>
+#include <string_view>
+#include <utility>
 
 namespace d3dutil {
+
+/// @brief Returns the size of a single pixel (in bytes) of the given format.
+/// @param[in] format the format to check
+/// @return the number of bytes per pixel of the format. Not all types are handled.
+FORCE_INLINE UINT GetFormatSize(DXGI_FORMAT format) {
+    switch (format) {
+    case DXGI_FORMAT_UNKNOWN: return 1;
+    case DXGI_FORMAT_R8G8B8A8_UINT: return 4;
+    case DXGI_FORMAT_R8G8B8A8_UNORM: return 4;
+    case DXGI_FORMAT_R32_TYPELESS: return 4;
+    case DXGI_FORMAT_R32G32_UINT: return 8;
+    default: YMIR_DEV_CHECK(); return 0;
+    }
+}
 
 /// @brief Sets a debug name for a D3D11 resource.
 /// The name is displayed on tools like RenderDoc.
@@ -55,23 +74,45 @@ FORCE_INLINE void SafeRelease(std::array<T *, N> &objects) {
     }
 }
 
+/// @brief Safely releases a vector of objects, handling `nullptr`s gracefully.
+///
+/// @tparam T the type of objects in the vector
+/// @param[in] objects the vector of objects to release. The vector is cleared afterwards.
+template <typename T>
+    requires std::derived_from<T, IUnknown>
+FORCE_INLINE void SafeRelease(std::vector<T *> &objects) {
+    for (auto &object : objects) {
+        if (object != nullptr) {
+            object->Release();
+        }
+    }
+    objects.clear();
+}
+
 /// @brief Tracks dirty bits and allows processing ranges of dirty bits.
 /// @tparam numBits the number of bits in the bitmap
 template <size_t numBits>
 struct DirtyBitmap {
+    using TEntry = uint64;
+    static constexpr size_t kBitsPerEntry = sizeof(TEntry) * 8;
+    static constexpr size_t kEntryMask = kBitsPerEntry - 1;
+    static constexpr size_t kEntryShift = std::countr_zero(kBitsPerEntry);
+    static constexpr size_t kNumEntries = (numBits + kBitsPerEntry - 1) >> kEntryShift;
+    static constexpr TEntry kAllBits = ~static_cast<TEntry>(0);
+
     /// @brief Sets the specified bit as dirty.
     /// @param[in] index the bit to set
-    void Set(uint64 index) {
+    void Set(TEntry index) {
         if (index < numBits) {
-            m_bitmap[index / kBitsPerEntry] |= 1ull << (index & 63ull);
+            m_bitmap[index >> kEntryShift] |= 1ull << (index & kEntryMask);
         }
     }
 
     /// @brief Sets all bits as dirty.
     void SetAll() {
-        m_bitmap.fill(0xFFFFFFFF'FFFFFFFF);
-        if constexpr ((numBits & 63) != 0) {
-            m_bitmap.back() = 0xFFFFFFFF'FFFFFFFFull >> (-numBits & 63ull);
+        m_bitmap.fill(kAllBits);
+        if constexpr ((numBits & kEntryMask) != 0) {
+            m_bitmap.back() = kAllBits >> (-numBits & kEntryMask);
         }
     }
 
@@ -83,7 +124,7 @@ struct DirtyBitmap {
     /// @brief Checks if any bit is set in the bitmap.
     /// @return `true` if any bit is set
     bool AnySet() const {
-        for (uint64 entry : m_bitmap) {
+        for (TEntry entry : m_bitmap) {
             if (entry != 0) {
                 return true;
             }
@@ -99,15 +140,15 @@ struct DirtyBitmap {
     /// @brief Processes all dirty bits and clears the bitmap.
     /// @tparam Fn the type of function that processes the bit ranges.
     /// @param[in] fn a function that processes the dirty bit ranges. The function is invoked with two parameters:
-    /// `uint64` offset from zero of the current dirty bit range, `uint64` count of set bits in the position.
+    /// `TEntry` offset from zero of the current dirty bit range, `TEntry` count of set bits in the position.
     template <typename Fn>
-        requires std::is_invocable_v<Fn, uint64 /*offset from zero*/, uint64 /*contiguous dirty bits set count*/>
+        requires std::is_invocable_v<Fn, TEntry /*offset from zero*/, TEntry /*contiguous dirty bits set count*/>
     void Process(Fn &&fn) {
-        uint64 offset = 0;
-        uint64 accumOnes = 0;
+        TEntry offset = 0;
+        TEntry accumOnes = 0;
         std::size_t i = 0;
-        uint64 entry = m_bitmap[0];
-        uint64 remaining = std::min<uint64>(64, numBits);
+        TEntry entry = m_bitmap[0];
+        TEntry remaining = std::min<TEntry>(kBitsPerEntry, numBits);
         while (i < kNumEntries) {
             // Zeros search phase
             while (entry == 0) {
@@ -117,18 +158,18 @@ struct DirtyBitmap {
                     break;
                 }
                 entry = m_bitmap[i];
-                remaining = 64;
+                remaining = kBitsPerEntry;
                 continue;
             }
 
-            const uint64 zeros = std::min<uint64>(std::countr_zero(entry), remaining);
+            const TEntry zeros = std::min<TEntry>(std::countr_zero(entry), remaining);
             offset += zeros;
             remaining -= zeros;
             entry >>= zeros;
 
             // Ones search phase
             while (true) {
-                const uint64 ones = std::countr_one(entry);
+                const TEntry ones = std::countr_one(entry);
                 accumOnes += ones;
                 entry >>= ones;
                 remaining -= ones;
@@ -143,7 +184,7 @@ struct DirtyBitmap {
                     break;
                 }
                 entry = m_bitmap[i];
-                remaining = 64;
+                remaining = kBitsPerEntry;
             }
         }
         if (accumOnes != 0) {
@@ -154,9 +195,7 @@ struct DirtyBitmap {
     }
 
 private:
-    static constexpr size_t kBitsPerEntry = sizeof(uint64) * 8;
-    static constexpr size_t kNumEntries = (numBits + kBitsPerEntry - 1) / kBitsPerEntry;
-    alignas(16) std::array<uint64, kNumEntries> m_bitmap = {};
+    alignas(16) std::array<TEntry, kNumEntries> m_bitmap = {};
 };
 
 } // namespace d3dutil
