344b127681d4e3e7c3df4884be28282772a6e0c4
StrikerX3 <StrikerX3@users.noreply.github.com>
2026-02-27 18:41:47 -0300

    chore(d3d11): Remove unused atlas defs

--- libs/ymir-core/src/ymir/hw/vdp/renderer/d3d11/d3d11_defs.hpp
+++ libs/ymir-core/src/ymir/hw/vdp/renderer/d3d11/d3d11_defs.hpp
@@ -12,12 +12,6 @@ inline constexpr uint32 kVDP1FBRAMPages = kVDP1FramebufferRAMSize >> kVRAMPageBi
 inline constexpr uint32 kVDP1VRAMPages = kVDP1VRAMSize >> kVRAMPageBits;
 inline constexpr uint32 kVDP2VRAMPages = kVDP2VRAMSize >> kVRAMPageBits;
 
-inline constexpr uint32 kVDP1PolyAtlasH = 2048;
-inline constexpr uint32 kVDP1PolyAtlasV = 2048;
-
-static_assert(kVDP1PolyAtlasH >= kVDP1MaxFBSizeH);
-static_assert(kVDP1PolyAtlasV >= kVDP1MaxFBSizeV);
-
 inline constexpr uint32 kColorCacheSize = kVDP2CRAMSize / sizeof(uint16); // in color entries
 inline constexpr uint32 kCoeffCacheSize = kVDP2CRAMSize / 2;              // in bytes; top-half only
 
