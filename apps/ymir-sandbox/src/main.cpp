d918c83861245af9ea27c2a5c78b8e04a8b479a2
StrikerX3 <StrikerX3@users.noreply.github.com>
2026-06-19 13:11:37 -0300

    feat(sandbox): SCSP deadlock tester

--- apps/ymir-sandbox/src/main.cpp
+++ apps/ymir-sandbox/src/main.cpp
@@ -1,6 +1,7 @@
 ﻿#include <ymir/util/scope_guard.hpp>
 
 #include <ymir/sys/backup_ram.hpp>
+#include <ymir/sys/saturn.hpp>
 
 #include <ymir/hw/sh2/sh2.hpp>
 #include <ymir/hw/vdp/vdp.hpp>
@@ -1746,6 +1747,54 @@ void runDiscInfoExtractor(int argc, char **argv) {
     }
 }
 
+void runDeadlockTest(int argc, char **argv) {
+    if (argc < 2) {
+        fmt::println("missing path argument");
+        return;
+    }
+
+    std::filesystem::path path{argv[1]};
+
+    std::vector<uint8> ipl{};
+    if (path.empty()) {
+        fmt::println("No IPL ROM provided");
+        return;
+    }
+
+    constexpr auto romSize = ymir::sys::kIPLSize;
+    std::ifstream stream{path, std::ios::binary | std::ios::ate};
+    if (stream.is_open()) {
+        auto size = stream.tellg();
+        stream.seekg(0, std::ios::beg);
+        ipl.resize(size);
+        stream.read(reinterpret_cast<char *>(ipl.data()), size);
+    }
+    if (ipl.size() != ymir::sys::kIPLSize) {
+        fmt::println("Invalid IPL ROM size: {} bytes (expected {} bytes)", ipl.size(), romSize);
+        return;
+    }
+
+    using clk = std::chrono::steady_clock;
+    uint64 iter = 0;
+    for (;;) {
+        auto sat = std::make_unique<ymir::Saturn>();
+        sat->configuration.audio.threadedSCSP = true;
+        sat->configuration.video.threadedVDP1 = true;
+        sat->configuration.video.threadedVDP2 = true;
+        sat->configuration.video.threadedDeinterlacer = true;
+        sat->LoadIPL(std::span<uint8, ymir::sys::kIPLSize>(ipl));
+
+        const auto t0 = clk::now();
+        for (uint64 frames = 0; frames < 500; frames++) {
+            sat->RunFrame();
+        }
+        const auto t1 = clk::now();
+        const auto dt = t1 - t0;
+        fmt::println("iteration {} succeeded in {}", iter, dt);
+        ++iter;
+    }
+}
+
 int main(int argc, char **argv) {
     // runSandbox();
     // runBUPSandbox();
@@ -1764,7 +1813,8 @@ int main(int argc, char **argv) {
     // }
     // runCurlSandbox();
     // runSH2PerfSandbox();
-    runDiscInfoExtractor(argc, argv);
+    // runDiscInfoExtractor(argc, argv);
+    runDeadlockTest(argc, argv);
 
     return EXIT_SUCCESS;
 }
