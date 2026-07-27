04e5477bc1a4e9eb868291cbe6ccfe2150107fef
StrikerX3 <StrikerX3@users.noreply.github.com>
2026-07-27 13:34:28 -0300

    feat(audio): Auto-define `STB_VORBIS_HEADER_ONLY` to simplify stb_vorbis usage

--- vendor/stb/ymir/stb_implementations.cpp
+++ vendor/stb/ymir/stb_implementations.cpp
@@ -4,5 +4,8 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb_image_write.h>
 
-// stb_vorbis.c is backwards and provides the implementation by default.
-// Users MUST define STB_VORBIS_HEADER_ONLY before including the combined header-source file.
+// stb_vorbis.c is backwards and provides the implementation by default, with a macro to disable it.
+// Ymir's CMakeLists.txt for stb already defines it publicly so that library consumers can simply include the header and
+// not worry about duplicate implementations or defining the header-only macro.
+#undef STB_VORBIS_HEADER_ONLY
+#include <stb_vorbis.c>
