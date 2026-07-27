613913f58a066e6a71d76027f2b6e5cbad7730f7
StrikerX3 <StrikerX3@users.noreply.github.com>
2026-07-27 10:29:24 -0300

    build: Use stb as submodule and add dr_libs
    This allows `ymir-core` to use them as dependencies

---
+++ vendor/stb/ymir/stb_implementations.cpp
@@ -0,0 +1,8 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
+
+// stb_vorbis.c is backwards and provides the implementation by default.
+// Users MUST define STB_VORBIS_HEADER_ONLY before including the combined header-source file.
